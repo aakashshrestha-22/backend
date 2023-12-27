a14577d4833664df200ce8901bc9f3a2ca01837f
aakash-22ak <akashshrestha678@gmail.com>
2023-12-27 12:34:30 +0545

    bcrypt and jwt

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// require("dotenv").config({ path: "./env" });
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+dotenv.config({
+  path: "./env",
+});
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed", err);
+  });
